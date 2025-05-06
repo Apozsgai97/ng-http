29faf20c0cd35931bffd618ef6d5faba193ff6e6
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-05-06 14:47:26 +0200

    Implement removeUserPlace method in PlacesService and update UserPlacesComponent to handle place removal

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -3,6 +3,7 @@ import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
 import { PlacesService } from '../places.service';
+import { Place } from '../place.model';
 
 @Component({
   selector: 'app-user-places',
@@ -31,6 +32,15 @@ export class UserPlacesComponent implements OnInit {
             this.isFetching.set(false);
           },
         });
+      this.destroyRef.onDestroy(() => {
+        subscription.unsubscribe();
+      });
+    }
+
+  onSelectPlace(place: Place) {
+      const subscription = this.placesService
+        .removeUserPlace(place)
+        .subscribe();
       this.destroyRef.onDestroy(() => {
         subscription.unsubscribe();
       });
