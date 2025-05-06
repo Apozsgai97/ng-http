b1070058ea2adb01e21c05c2ef3adff25c2ba70a
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-05-06 11:59:18 +0200

    Add user places fetching logic and error handling in UserPlacesComponent

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { Place } from '../place.model';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-user-places',
@@ -10,5 +13,34 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit {
+  isFetching = signal(false);
+  error = signal('');
+  userPlaces = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+  
+  ngOnInit() {
+      this.isFetching.set(true);
+      const subscription = this.httpClient
+        .get<{ places: Place[] }>('http://localhost:3000/user-places')
+        .pipe(map((resData) => resData.places))
+        .subscribe({
+          next: (places) => {
+            this.userPlaces.set(places);
+          },
+          error: (error) => {
+            console.error(error);
+            this.error.set(
+              'Something went wrong while fetching your favorite places. Please try again later.'
+            );
+          },
+          complete: () => {
+            this.isFetching.set(false);
+          },
+        });
+      this.destroyRef.onDestroy(() => {
+        subscription.unsubscribe();
+      });
+    }
 }
